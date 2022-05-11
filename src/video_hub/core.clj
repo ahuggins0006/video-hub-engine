f8495ffb1ce9b4fc2a253934423423b691efb91f
Andrew Huggins <ahuggins@torchtechnologies.com>
2022-05-11 17:12:45 -0500

    added layout namespace responsible for layout functionality

--- src/video_hub/core.clj
+++ src/video_hub/core.clj
@@ -6,10 +6,18 @@
    [manifold.deferred :as d]
    [manifold.stream :as s]
    [clojure.string :as str]
+   [clojure.java.shell :as shell]
+   [video-hub.layout :as lo]
    )
   (:gen-class))
 
-(def protocol (gloss/compile-frame (gloss/string :ascii)))
+(def protocol (gloss/compile-frame (gloss/string :ascii :delimeters ["\n\n"])))
+(def protocol (gloss/compile-frame (gloss/string :ascii :delimeters [])))
+
+(def dlm-rep-string
+  (gloss/repeated
+    (gloss/string :utf-8 :delimiters ["\n\n"])
+    :delimiters ["\n\n"]))
 
 (defn wrap-duplex-stream
   [protocol s]
@@ -31,14 +39,90 @@
 (def c1 (try-client "10.10.0.30" 9990))
 c1
 
-(def simple-routing "VIDEO OUTPUT ROUTING:\n 0 3\n")
-(s/put! c1 simple-routing)
-(s/take! c1)
 
+(def simple-routing "VIDEO OUTPUT ROUTING:\n 0 3\n\n")
+(def simple-routing "VIDEO OUTPUT ROUTING:\n 0 3 \n 2 5\n\n")
+(def req-output-routing "VIDEO OUTPUT ROUTING:\n\n")
+(def req-output-labels "OUTPUT LABELS:\n\n")
+(def ping "PING:\n\n")
+(s/put! c1 simple-routing)
+(s/put! c1 req-output-routing)
+(s/put! c1 req-output-labels)
+(s/put! c1 ping)
+
+
+
+(s/put! c1 lo/multi-layout-req)
+;; network connections
+
+(s/consume #(prn 'message! %) c1)
+
+;;test periodically
+(def status-period (s/periodically 2000 #(s/try-put! c1 req-output-labels 1000)))
+(s/consume #(prn 'message! %) status-period)
+
+(s/consume #(println %) status-period)
+
+(defn ping-something [something]
+  (case (.toLowerCase (System/getProperty "os.name"))
+    "linux"                     (#(if (= (second %) 0) true false) (first (shell/sh "ping" "-c" "1" "-W" "3" something)))
+    ("windows 10"
+     "windows 11") (str/includes? (:out (shell/sh "cmd" "/C" "powershell.exe" "Test-Connection" something "-Quiet" "-Count" "1")) "True")))
+
+(def ping-period (s/periodically 1000 #(ping-something "google.com")))
+
+(s/consume #(prn 'message! %) ping-period)
+
+
+(s/close! c1)
+(s/close! ping-period)
+(s/close! status-period)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
 
+(comment message! "PROTOCOL PREAMBLE:\nVersion: 2.7\n\nVIDEOHUB DEVICE:\nDevice present: true\nModel name: Blackmagic Smart Videohub 20 x 20\nFriendly name: Smart Videohub 20 x 20\nUnique ID: 7C2E0DA55C76\nVideo inputs: 20\nVideo processing units: 0\nVideo outputs: 20\nVideo monitoring outputs: 0\nSerial ports: 0\n\nINPUT LABELS:\n0 Input 1\n1 Input 2\n2 Input 3\n3 Input 4\n4 Input 5\n5 Input 6\n6 Input 7\n7 Input 8\n8 Input 9\n9 Input 10\n10 Input 11\n11 Input 12\n12 Input 13\n13 Input 14\n14 Input 15\n15 Input 16\n16 Input 17\n17 Input 18\n18 Input 19\n19 Input 20\n\nOUTPUT LABELS:\n0 Output 1\n1 Output 2\n2 Output 3\n3 Output 4\n4 Output 5\n5 Output 6\n6 Output 7\n7 Output 8\n8 Output 9\n9 Output 10\n10 Output 11\n11 Output 12\n12 Output 13\n13 Output 14\n14 Output 15\n15 Output 16\n16 Output 17\n17 Output 18\n18 Output 19\n19 Output 20\n\nVIDEO OUTPUT LOCKS:\n0 U\n1 U\n2 U\n3 U\n4 U\n5 U\n6 U\n7 U\n8 U\n9 U\n10 U\n11 U\n12 U\n13 U\n14 U\n15 U\n16 U\n17 U\n18 U\n19 U\n\nVIDEO OUTPUT ROUTING:\n0 3\n1 1\n2 1\n3 1\n4 1\n5 1\n6 1\n7 1\n8 1\n9 1\n10 1\n11 1\n12 1\n13 1\n14 1\n15 1\n16 1\n17 1\n18 1\n19 0\n\nCONFIGURATION:\nTake Mode: true\n\nEND PRELUDE:\n\n"
+message! "ACK\n\nVIDEO OUTPUT ROUTING:\n0 3\n1 1\n2 1\n3 1\n4 1\n5 1\n6 1\n7 1\n8 1\n9 1\n10 1\n11 1\n12 1\n13 1\n14 1\n15 1\n16 1\n17 1\n18 1\n19 0\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "OUTPUT LABELS:\n0 Output 1\n1 Output 2\n2 Output 3\n3 Output 4\n4 Output 5\n5 Output 6\n6 Output 7\n7 Output 8\n8 Output 9\n9 Output 10\n10 Output 11\n11 Output 12\n12 Output 13\n13 Output 14\n14 Output 15\n15 Output 16\n16 Output 17\n17 Output 18\n"
+message! "OUTPUT LABELS:\n0 Output 1\n1 Output 2\n2 Output 3\n3 Output 4\n4 Output 5\n5 Output 6\n6 Output 7\n7 Output 8\n8 Output 9\n9 Output 10\n10 Output 11\n11 Output 12\n12 Output 13\n13 Output 14\n14 Output 15\n15 Output 16\n16 Output 17\n17 Output 18\n"
+message! "18 Output 19\n19 Output 20\n\n"
+message! "18 Output 19\n19 Output 20\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "OUTPUT LABELS:\n0 Output 1\n1 Output 2\n2 Output 3\n3 Output 4\n4 Output 5\n5 Output 6\n6 Output 7\n7 Output 8\n8 Output 9\n9 Output 10\n10 Output 11\n11 Output 12\n12 Output 13\n13 Output 14\n14 Output 15\n15 Output 16\n16 Output 17\n17 Output 18\n"
+message! "OUTPUT LABELS:\n0 Output 1\n1 Output 2\n2 Output 3\n3 Output 4\n4 Output 5\n5 Output 6\n6 Output 7\n7 Output 8\n8 Output 9\n9 Output 10\n10 Output 11\n11 Output 12\n12 Output 13\n13 Output 14\n14 Output 15\n15 Output 16\n16 Output 17\n17 Output 18\n"
+message! "18 Output 19\n19 Output 20\n\n"
+message! "18 Output 19\n19 Output 20\n\n"
+message! "PROTOCOL PREAMBLE:\nVersion: 2.7\n\nVIDEOHUB DEVICE:\nDevice present: true\nModel name: Blackmagic Smart Videohub 20 x 20\nFriendly name: Smart Videohub 20 x 20\nUnique ID: 7C2E0DA55C76\nVideo inputs: 20\nVideo processing units: 0\nVideo outputs: 20\nVideo monitoring outputs: 0\nSerial ports: 0\n\nINPUT LABELS:\n0 Input 1\n1 Input 2\n2 Input 3\n3 Input 4\n4 Input 5\n5 Input 6\n6 Input 7\n7 Input 8\n8 Input 9\n9 Input 10\n10 Input 11\n11 Input 12\n12 Input 13\n13 Input 14\n14 Input 15\n15 Input 16\n16 Input 17\n17 Input 18\n18 Input 19\n19 Input 20\n\n"
+message! "OUTPUT LABELS:\n0 Output 1\n1 Output 2\n2 Output 3\n3 Output 4\n4 Output 5\n5 Output 6\n6 Output 7\n7 Output 8\n8 Output 9\n9 Output 10\n10 Output 11\n11 Output 12\n12 Output 13\n13 Output 14\n14 Output 15\n15 Output 16\n16 Output 17\n17 Output 18\n"
+message! "18 Output 19\n19 Output 20\n\nVIDEO OUTPUT LOCKS:\n0 U\n1 U\n2 U\n3 U\n4 U\n5 U\n6 U\n7 U\n8 U\n9 U\n10 U\n11 U\n12 U\n13 U\n14 U\n15 U\n16 U\n17 U\n18 U\n19 U\n\nVIDEO OUTPUT ROUTING:\n0 3\n1 1\n2 1\n3 1\n4 1\n5 1\n6 1\n7 1\n8 1\n9 1\n10 1\n11 1\n12 1\n13 1\n14 1\n15 1\n16 1\n17 1\n18 1\n19 0\n\nCONFIGURATION:\nTake Mode: true\n\nEND PRELUDE:\n\n"
+message! "ACK\n\n"
+message! "OUTPUT LABELS:\n0 Output 1\n1 Output 2\n2 Output 3\n3 Output 4\n4 Output 5\n5 Output 6\n6 Output 7\n7 Output 8\n8 Output 9\n9 Output 10\n10 Output 11\n11 Output 12\n12 Output 13\n13 Output 14\n14 Output 15\n15 Output 16\n16 Output 17\n17 Output 18\n"
+message! "18 Output 19\n19 Output 20\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "VIDEO OUTPUT ROUTING:\n0 3\n1 1\n2 1\n3 1\n4 1\n5 1\n6 1\n7 1\n8 1\n9 1\n10 1\n11 1\n12 1\n13 1\n14 1\n15 1\n16 1\n17 1\n18 1\n19 0\n\n"
+message! "ACK\n\n"
+message! "VIDEO OUTPUT ROUTING:\n0 3\n1 1\n2 1\n3 1\n4 1\n5 1\n6 1\n7 1\n8 1\n9 1\n10 1\n11 1\n12 1\n13 1\n14 1\n15 1\n16 1\n17 1\n18 1\n19 0\n\n"
+message! "ACK\n\n"
+message! "VIDEO OUTPUT ROUTING:\n0 3\n1 1\n2 1\n3 1\n4 1\n5 1\n6 1\n7 1\n8 1\n9 1\n10 1\n11 1\n12 1\n13 1\n14 1\n15 1\n16 1\n17 1\n18 1\n19 0\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "ACK\n\n"
+message! "OUTPUT LABELS:\n0 Output 1\n1 Output 2\n2 Output 3\n3 Output 4\n4 Output 5\n5 Output 6\n6 Output 7\n7 Output 8\n8 Output 9\n9 Output 10\n10 Output 11\n11 Output 12\n12 Output 13\n13 Output 14\n14 Output 15\n15 Output 16\n16 Output 17\n17 Output 18\n"
+message! "18 Output 19\n19 Output 20\n\n"
+message! "ACK\n\n"
+message! "VIDEO OUTPUT ROUTING:\n0 3\n1 1\n2 1\n3 1\n4 1\n5 1\n6 1\n7 1\n8 1\n9 1\n10 1\n11 1\n12 1\n13 1\n14 1\n15 1\n16 1\n17 1\n18 1\n19 0\n\n"
+message! "ACK\n\n"
+)
 
---
+++ src/video_hub/layout.clj
@@ -0,0 +1,60 @@
+(ns video-hub.layout
+ (:require
+  [clojure.string :as str]
+ ))
+
+
+(defn route-cmd->req
+  [output input]
+  (let [out (str (dec output))
+        in  (str (dec input))]
+    (apply str out " " in "\n"))
+  )
+
+_#VIDEO OUTPUT ROUTING:  "\n\n"
+(route-cmd->req 1 4)
+;; => "\n0 3\n"
+
+;; build layout/scene map
+
+(def louts {:layouts
+            {:l1 {:r1 {:out 1
+                       :in  4}
+                  :r2 {:out 2
+                       :in 5}
+                  }}})
+
+(-> louts :layouts)
+;; => {:l1 {:r1 {:out 1, :in 4}, :r2 {:out 2, :in 5}}}
+
+
+(defn layout->routes
+  [layout]
+  (let [routes (vals layout)]
+    (vals (first routes))
+    )
+  )
+
+(layout->routes (:layouts louts))
+;; => ({:out 1, :in 4} {:out 2, :in 5})
+
+(map #(route-cmd->req (:out %) (:in %))(layout->routes (:layouts louts)))
+;; => ("VIDEO OUTPUT ROUTING:\n0 3\n\n" "VIDEO OUTPUT ROUTING:\n1 4\n\n")
+
+(apply str (map #(route-cmd->req (:out %) (:in %))(layout->routes (:layouts louts))))
+;; => "VIDEO OUTPUT ROUTING:\n0 3\n\nVIDEO OUTPUT ROUTING:\n1 4\n\n"
+
+(defn layout->routes-reqs
+  [layout]
+  (let [routes (->> layout
+                    vals
+                    first
+                    layout->routes)
+        reqs   (->> routes
+                    (map #(route-cmd->req (:out %) (:in %)))
+                    (apply str))]
+    (apply str "VIDEO OUTPUT ROUTING:\n" reqs "\n")
+
+    ))
+
+(def multi-layout-req (layout->routes-reqs louts))
